644e1058856d1daaa4d30d23822f5624d490ea0f
Pedro Kalil <kalilgamer0@gmail.com>
2018-04-26 22:28:15 -0300

    Makefile: remove guard makefile

--- Android.mk
+++ Android.mk
@@ -1,3 +1,3 @@
-ifeq ($(call my-dir),$(call project-path-for,qcom-dataservices))
+#ifeq ($(call my-dir),$(call project-path-for,qcom-dataservices))
 include $(call all-subdir-makefiles)
-endif
+#endif
